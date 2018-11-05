e95d8ae6b69f005bcc34a1f73de907cc6815cc06
Hirofumi Tanigami <tanigami@gmail.com>
2018-11-06 02:17:55 +0900

    some logs

--- src/Application/Command/SendFreshNotificationsHandler.php
+++ src/Application/Command/SendFreshNotificationsHandler.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 
 namespace Shippinno\Notification\Application\Command;
 
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Shippinno\Notification\Domain\Model\NotificationRepository;
 use Shippinno\Notification\Domain\Model\SendNotification as SendNotificationService;
 
 class SendFreshNotificationsHandler
 {
+    use LoggerAwareTrait;
+
     /**
      * @var NotificationRepository
      */
@@ -21,13 +26,16 @@ class SendFreshNotificationsHandler
     /**
      * @param NotificationRepository $notificationRepository
      * @param SendNotificationService $sendNotificationService
+     * @param LoggerInterface|null $logger
      */
     public function __construct(
         NotificationRepository $notificationRepository,
-        SendNotificationService $sendNotificationService
+        SendNotificationService $sendNotificationService,
+        LoggerInterface $logger = null
     ) {
         $this->notificationRepository = $notificationRepository;
         $this->sendNotificationService = $sendNotificationService;
+        $this->logger = is_null($logger) ? new NullLogger : $logger;
     }
 
     /**
@@ -36,9 +44,15 @@ class SendFreshNotificationsHandler
     public function handle(SendFreshNotifications $command): void
     {
         $notifications = $this->notificationRepository->freshNotifications();
+        $this->logger->debug(sprintf('Sending %s fresh notifications.', count($notifications)));
+        $sent = 0;
         foreach ($notifications as $notification) {
             $this->sendNotificationService->execute($notification);
+            if ($notification->isSent()) {
+                $sent = $sent + 1;
+            }
             $this->notificationRepository->persist($notification);
         }
+        $this->logger->debug(sprintf('Sent %s notifications.', $sent));
     }
 }
